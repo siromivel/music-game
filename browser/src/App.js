91573b71dfa0009a533cc42ab59202a7aa39acd5
Levi Morris <siromivel@gmail.com>
2019-03-13 18:52:47 -0600

    Add random cat headers

--- browser/src/App.js
+++ browser/src/App.js
@@ -2,10 +2,16 @@ import React, { Component } from "react"
 import "./styles/App.css"
 
 class App extends Component {
+    randomCatGif() {
+        let cats = ["bongo", "keyboard", "pixel", "whistle"]
+        return `../assets/${cats[Math.floor(Math.random() * cats.length)]}cat.gif`
+    }
+
     render() {
         return (
             <div className="main">
-                <h1>THIS. IS. BONGOCOIN.</h1>
+                <h1 className="text-center">Welcome to Music School</h1>
+                <img className="cat-header" src={this.randomCatGif()} />
             </div>
         )
     }
--- browser/src/web3/Web3Provider.js
+++ browser/src/web3/Web3Provider.js
@@ -1,11 +1,7 @@
 import { ethers } from "ethers"
 
-export default class Web3 {
+export class Web3 {
     constructor(network) {
-        if (process.env === "development") {
-            this.provider = new ethers.providers.JsonRpcProvider(network)
-        } else {
-            this.provider = new ethers.providers.Web3Provider(network)
-        }
+        this.provider = new ethers.providers.JsonRpcProvider(network)
     }
 }
