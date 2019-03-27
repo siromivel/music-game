d8981b90bc61edb130c113c27fdd96b2d7f004af
Levi Morris <siromivel@gmail.com>
2019-03-27 11:41:43 -0600

    Add missing enum

---
+++ browser/src/constants/SymbolToGifMap.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var SymbolToGifMap;
+(function (SymbolToGifMap) {
+    SymbolToGifMap["BON"] = "bongo";
+    SymbolToGifMap["FID"] = "fiddle";
+    SymbolToGifMap["KEY"] = "keyboard";
+    SymbolToGifMap["WHI"] = "whistle";
+})(SymbolToGifMap = exports.SymbolToGifMap || (exports.SymbolToGifMap = {}));
