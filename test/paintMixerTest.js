d28186bfe3d47d56e1191fe3fa90d692ca8ced78
Levi Morris <siromivel@gmail.com>
2019-03-27 17:05:07 -0600

    Add color value padding algo, fix test file name

--- test/paintMixerTest.js
+++ test/paintMixerTest.js
@@ -16,13 +16,21 @@ contract("Paint mixer", function([owner, ...otherAddresses]) {
     it("returns an array of four random valid hex color values", async function() {
         const result =  await this.paintMixer
                                   .mixPaint(owner, otherAddresses[0], 2)
-                                  .then(r => r.map(mapUint24ToHexColorString))
+                                  .then(r => r.map(mapUint24ToHexColorCode))
 
         result.length.should.equal(4)
-        result.forEach(val => /^#[0-9A-F]{6}$/.test(val).should.be.true)
+        result.forEach((val) => /^#[0-9A-F]{6}$/.test(val).should.be.true)
     })
 })
 
-function mapUint24ToHexColorString(uint24) {
-    return "#" + uint24.words[0].toString(16).toUpperCase()
+function mapUint24ToHexColorCode(uint24) {
+    const hexStr = uint24.words[0].toString(16)
+    const len = hexStr.length
+    let rgb = []
+
+    for (let i = 0; i < len; i += 2) {
+        rgb.push(hexStr.slice(i, i + 2).padStart(2, "0"))
+    }
+
+    return "#" + rgb.join("").toUpperCase()
 }
