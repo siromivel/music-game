9db2301275f10b044e48f6c31a93e7ead8a21c5b
Levi Morris <siromivel@gmail.com>
2019-03-27 20:37:40 -0600

    WIP: Build out "Instrument" data type

--- contracts/InstrumentToken.sol
+++ contracts/InstrumentToken.sol
@@ -2,20 +2,30 @@ pragma solidity ^0.5.0;
 
 import "openzeppelin-solidity/contracts/token/ERC721/ERC721Full.sol";
 
+interface PaintMixer {
+    function mixPaint(uint256 _primary, uint256 _secondary, uint256 _targetBlock) external view returns(uint24[] memory);
+}
+
 contract InstrumentToken is ERC721Full {
-    uint lastTokenId;
+    uint lastTokenId = 0;
 
     constructor(string memory _name, string memory _symbol) ERC721Full(_name, _symbol) public {
-        lastTokenId = 0;
         _mint(msg.sender, lastTokenId);
     }
 
+    struct Instrument {
+        uint24 primary;
+        uint24 secondary;
+        uint24 tertiary;
+        uint24 quaternary;
+    }
+
     function musicLesson(address _student) public returns (bool) {
-        require(balanceOf(msg.sender) > 0, "Only a master can give lessons");
-        require(balanceOf(_student) == 0, "You cannot teach a master");
+        require(balanceOf(msg.sender) > 0, "REQUIRES_INSTRUMENT");
+        require(balanceOf(_student) == 0, "STUDENT_ALREADY_TAUGHT");
+
         lastTokenId += 1;
         _mint(_student, lastTokenId);
-
         lastTokenId += 1;
         _mint(msg.sender, lastTokenId);
 
