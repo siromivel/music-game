0e5c3b9d155c88351cf548f1bfae14e89fd4c04d
Levi Morris <siromivel@gmail.com>
2019-03-31 13:34:08 -0600

    Initial PaintMixer integration
    - Instruments now have a cooldown before they can be used to generate children.
    - Teaching always uses your newest instrument; any given instrument can only be used for creating a new instrument once.

--- contracts/InstrumentToken.sol
+++ contracts/InstrumentToken.sol
@@ -7,9 +7,28 @@ interface PaintMixer {
 }
 
 contract InstrumentToken is ERC721Full {
-    uint lastTokenId = 0;
+    address owner;
+    PaintMixer paintMixer;
 
-    constructor(string memory _name, string memory _symbol) ERC721Full(_name, _symbol) public {
+    uint lastTokenId = 0;
+    Instrument[] instruments;
+
+    constructor(
+        string memory _name,
+        string memory _symbol,
+        address _paintMixer
+    ) ERC721Full(_name, _symbol) public {
+        owner = msg.sender;
+        setPaintMixerAddress(_paintMixer);
+
+        uint24[] memory colors = new uint24[](4);
+        colors[0] = uint24(1);
+        colors[1] = uint24(1);
+        colors[2] = uint24(1);
+        colors[3] = uint24(1);
+
+        Instrument memory genesis = _createInstrument(colors);
+        instruments.push(genesis);
         _mint(msg.sender, lastTokenId);
     }
 
@@ -18,22 +37,74 @@ contract InstrumentToken is ERC721Full {
         uint24 secondary;
         uint24 tertiary;
         uint24 quaternary;
+        uint160 nextProductionBlock; // This will be fine until block # ~ 1.46x10^48, it'll be a while even if block times drop to 5s :)
+    }
+
+    modifier _onlyOwner {
+        require(msg.sender == owner);
+        _;
+    }
+
+    function _createInstrument(uint24[] memory _colors) internal view returns(Instrument memory) {
+        Instrument memory _instrument = Instrument({
+            primary: _colors[0],
+            secondary: _colors[1],
+            tertiary: _colors[2],
+            quaternary: _colors[3],
+            nextProductionBlock: uint160(block.number + block.number % 13)
+        });
+
+        return _instrument;
+    }
+
+    function setPaintMixerAddress(address _new) public _onlyOwner {
+        paintMixer = PaintMixer(_new);
+    }
+
+    function getTokensOfSender() public view returns(uint256[] memory) {
+        if (balanceOf(msg.sender) == 0) return new uint256[](0);
+        return _tokensOfOwner(msg.sender);
+    }
+
+    function getInstrument(
+        uint256 _id
+    ) public view returns (
+        uint24 primary,
+        uint24 secondary,
+        uint24 tertiary,
+        uint24 quaternary,
+        uint160 nextProductionBlock
+    ) {
+        Instrument memory instrument = instruments[_id];
+
+        return (instrument.primary, instrument.secondary, instrument.tertiary, instrument.quaternary, instrument.nextProductionBlock);
     }
 
     function musicLesson(address _student) public returns (bool) {
         require(balanceOf(msg.sender) > 0, "REQUIRES_INSTRUMENT");
-        require(balanceOf(_student) == 0, "STUDENT_ALREADY_TAUGHT");
+        require(balanceOf(_student) == 0, "STUDENT_MUST_NOT_HAVE_INSTRUMENT");
 
+        // Get parent token
+        uint256[] memory teacherTokens = getTokensOfSender();
+        uint256 parentToken = teacherTokens[teacherTokens.length - 1];
+        Instrument memory parentInstrument = instruments[parentToken];
+
+        // Create instrument for student
         lastTokenId += 1;
+        uint256 parentPaint = parentInstrument.primary + parentInstrument.secondary + parentInstrument.tertiary + parentInstrument.quaternary;
+        uint24[] memory studentColors = paintMixer.mixPaint(parentPaint, lastTokenId, parentInstrument.nextProductionBlock);
+        Instrument memory studentInstrument = _createInstrument(studentColors);
+        instruments.push(studentInstrument);
         _mint(_student, lastTokenId);
+
+        // Create instrument for teacher
         lastTokenId += 1;
+        uint256 studentPaint = studentInstrument.primary + studentInstrument.secondary + studentInstrument.tertiary + studentInstrument.quaternary;
+        uint24[] memory teacherColors = paintMixer.mixPaint(studentPaint, lastTokenId, parentInstrument.nextProductionBlock);
+        Instrument memory teacherInstrument = _createInstrument(teacherColors);
+        instruments.push(teacherInstrument);
         _mint(msg.sender, lastTokenId);
 
         return true;
-    }
-
-    function getTokensOfSender() external view returns(uint256[] memory) {
-        if (balanceOf(msg.sender) == 0) return new uint256[](0);
-        return _tokensOfOwner(msg.sender);
     }
 }
--- test/instrumentTokenTest.js
+++ test/instrumentTokenTest.js
@@ -8,41 +8,47 @@ const should = chai.should()
 chai.use(chaiAsPromised)
 
 const InstrumentToken = artifacts.require("InstrumentToken")
+const PaintMixer = artifacts.require("PaintMixer")
 
 contract("InstrumentToken", function([owner, ...otherAddresses]) {
     beforeEach(async function() {
-        this.instrumentToken = await InstrumentToken.new("Fiddle", "FID")
+        this.paintMixer = await PaintMixer.new()
+        this.instrumentToken = await InstrumentToken.new("Fiddle", "FID", this.paintMixer.address)
+
+        const genesisInstrument = await this.instrumentToken.getInstrument(0)
+        const targetBlock = genesisInstrument[4].words[0]
+        skipToBlock(targetBlock)
     })
 
     describe("musicLesson", function () {
         it("correctly grants tokens to the student and teacher", async function() {
             let studentBalance = await this.instrumentToken.balanceOf(otherAddresses[0])
             let teacherBalance = await this.instrumentToken.balanceOf(owner)
-            studentBalance.words[0].should.to.equal(0)
-            teacherBalance.words[0].should.to.equal(1)
+            studentBalance.words[0].should.equal(0)
+            teacherBalance.words[0].should.equal(1)
 
             await this.instrumentToken.musicLesson(otherAddresses[0])
-            
+
             studentBalance = await this.instrumentToken.balanceOf(otherAddresses[0])
             teacherBalance = await this.instrumentToken.balanceOf(owner)
-            studentBalance.words[0].should.to.equal(1)
-            teacherBalance.words[0].should.to.equal(2)
+            studentBalance.words[0].should.equal(1)
+            teacherBalance.words[0].should.equal(2)
         })
 
         it("only a wallet holding a token can give lessons", async function() {
             let studentBalance = await this.instrumentToken.balanceOf(otherAddresses[0])
             let teacherBalance = await this.instrumentToken.balanceOf(otherAddresses[1])
-            studentBalance.words[0].should.to.equal(0)
-            teacherBalance.words[0].should.to.equal(0)
+            studentBalance.words[0].should.equal(0)
+            teacherBalance.words[0].should.equal(0)
 
             await this.instrumentToken.musicLesson(otherAddresses[0], { from: otherAddresses[1] })
-                .should.be.rejectedWith(/Only a master can give lessons/)
+                .should.be.rejectedWith(/REQUIRES_INSTRUMENT/)
         })
 
         it("can only give lessons to a wallet with no tokens", async function() {
             await this.instrumentToken.musicLesson(otherAddresses[0])
             await this.instrumentToken.musicLesson(otherAddresses[0])
-                .should.be.rejectedWith(/You cannot teach a master/)
+                .should.be.rejectedWith(/STUDENT_MUST_NOT_HAVE_INSTRUMENT/)
         })
     })
 
@@ -63,4 +69,21 @@ contract("InstrumentToken", function([owner, ...otherAddresses]) {
 
 function mapSolidityUintArrToArr(arr) {
     return arr.map(val => val.words[0])
+}
+
+function send(method, params = []) {
+    const id = 0
+    const jsonrpc = "2.0"
+    web3.currentProvider.send({ id, jsonrpc, method, params }, () => {})
+}
+
+async function skipToBlock(blockNumber) {
+    const currentBlock = await web3.eth.getBlockNumber()
+    const blocksToMine = blockNumber - currentBlock
+
+    let mined = 0;
+    while (mined < blocksToMine) {
+        await send("evm_mine")
+        mined++
+    }
 }
